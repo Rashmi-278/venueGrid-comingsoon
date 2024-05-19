39337d43744127601bc256e2f9f6a1fdcb3aaa2b
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2024-05-19 17:23:23 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,13 @@ export default function Home() {
     <main className="bg-[#03040B] flex flex-col items-center justify-center p-10 min-h-screen">
       <div className="space-y-1">
         <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
-          Join the waitlist for my
+          Venue Grid
         </h2>
         <h1 className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">
-          Newsletter
+          Discover your perfect venue
+        </h1>
+        <h1 className="z-10 text-xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-gray-300 to-gray-800 animate-fade-in-3">
+          Coming Soon
         </h1>
       </div>
       <NewsletterForm />
